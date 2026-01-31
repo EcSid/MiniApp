c33ce75679735fdd1f63a3f7dd69353bd403b62e
Ecs1d <egorgorynyc033@gmail.com>
2026-01-31 12:59:03 +0300

    fix url server

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 dotenv.config()
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: process.env.CLIENT_URL }))
 app.use(express.json())
 app.use('/api', router)
 
